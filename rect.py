6691370d64115c17725ca9078867ad530626f315
kongxuanzhi <875248646@qq.com>
2015-11-10 18:05:28 +0800

    画个表

---
+++ rect.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+__author__ = '孔轩志'
+
+from turtle import *
+
+begin_fill()
+rt(36)
+fd(100)
+lt(144)
+fd(100)
+rt(72)
+fd(100)
+lt(144)
+fd(100)
+rt(72)
+fd(100)
+lt(144)
+fd(100)
+rt(72)
+fd(100)
+lt(144)
+fd(100)
+rt(72)
+fd(100)
+lt(144)
+fd(100)
+
+
+input()
+pause
